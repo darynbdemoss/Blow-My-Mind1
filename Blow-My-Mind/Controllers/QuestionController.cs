e3adb8560408493326f6fb6c03db51afea9babf0
Daryn DeMoss <darynbrennondemoss@gmail.com>
2020-06-23 22:41:17 -0400

    Add project files.

---
+++ Blow-My-Mind/Controllers/QuestionController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blow_My_Mind.Controllers
+{
+    public class QuestionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
